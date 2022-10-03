cd0d36fae9378cfd680286bced5b6bd55f590f96
Nicolas HORNUEL <nicolas.hornuel@gmail.com>
2022-10-03 13:18:19 +0200

    remove env

--- FrontEnd/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  discogsToken: "lFqQfxurDTjYekydiilIbmdglWEdZqWinZMEnugw",
-  youtubeToken: "AIzaSyDhkZWj-Pp9cPApBeWn94XO43gGpMCith0",
-  urlApiDiscogs: "https://api.discogs.com",
-  urlApiYoutube: "https://www.googleapis.com/youtube/v3/search?",
-  //urlBack: "http://localhost:5001/fourthproject-aff75/us-central1/app/fire",
-  urlBack: "https://us-central1-fourthproject-aff75.cloudfunctions.net/app/fire",
-};
