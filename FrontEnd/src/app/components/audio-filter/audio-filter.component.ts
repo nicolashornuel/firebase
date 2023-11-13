03c420fc440f29aff3c3a2c1bd53b0574518d5fa
Nicolas HORNUEL <nicolas.hornuel@gmail.com>
2023-11-13 07:40:20 +0100

    pad canvas

--- FrontEnd/src/app/components/audio-filter/audio-filter.component.ts
+++ FrontEnd/src/app/components/audio-filter/audio-filter.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+
+export interface Position {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-audio-filter',
@@ -6,37 +11,86 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./audio-filter.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AudioFilterComponent implements OnInit {
+export class AudioFilterComponent implements AfterViewInit {
 
-  @Input('source') audioCtx: AudioContext | undefined;
+  @Input('source') gainNode: GainNode | undefined;
+  @Input('context') audioCtx: AudioContext | undefined;
   private biquadFilter: BiquadFilterNode;
-  private control = {
-    type: {
-      name: 'biquadFilterType',
-      type: 'select',
-      options: ['allpass', 'bandpass', 'highpass', 'lowpass'],
-      value: 'allpass',
-      apply: (value: BiquadFilterType) => this.biquadFilter.type = value
-    },
-    frequency: {
-      name: 'biquadFilterFrequency',
-      type: 'number',
-      min: 0,
-      max: 24000,
-      step: 10,
-      value: 0,
-      apply: (value: number) => this.biquadFilter.frequency.setValueAtTime(value, this.audioCtx.currentTime)
-    },
+  isMoving: boolean = false;
+  @ViewChild('canvasfilter') canvasfilter: ElementRef<HTMLCanvasElement>;
+  private ctxCanvas: CanvasRenderingContext2D | null;
+  private FLOAT32_MAX = 3.4028234663852886e+38;
+  private MAX = 50;
+
+  @HostListener('window:mouseup')
+  @HostListener('window:touchend')
+  onClick(): void {
+    this.isMoving = false;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.canvasfilter.nativeElement.width = 100;
+    this.canvasfilter.nativeElement.height = 100;
+    this.biquadFilter = this.audioCtx.createBiquadFilter();
   }
 
-  change(value: any): void {
-    console.log(value);
+  onChangeType(filterTypeSelected: 'allpass' | 'bandpass' | 'highpass' | 'lowpass'): void {
+    if (filterTypeSelected == 'allpass') {
+      this.biquadFilter.disconnect(0);
+    }
+    this.biquadFilter.type = filterTypeSelected;
+    this.gainNode.connect(this.biquadFilter);
+    this.biquadFilter.connect(this.gainNode.context.destination)
+  }
+
+  onXYPad(event: MouseEvent, eventType: 'start' | 'move'): void {
+    switch (eventType) {
+      case 'start':
+        this.isMoving = true;
+        this.draw(event);
+        break;
+      case 'move':
+        this.draw(event);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private getCursorPosition(event: MouseEvent, canvas: ElementRef<HTMLCanvasElement>): Position {
+    const rect = canvas.nativeElement.getBoundingClientRect();
+    return {
+      x: event.x - rect.x,
+      y: event.y - rect.y
+    };
+  }
+
+  private draw(event: MouseEvent): void {
+    if (this.isMoving) {
+      const canvas: HTMLCanvasElement = this.canvasfilter.nativeElement;
+      const ctx: CanvasRenderingContext2D = this.canvasfilter.nativeElement.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = 'green';
+      const { x, y } = this.getCursorPosition(event, this.canvasfilter);
+      ctx.fillRect(x, y, 10, 10);
+      this.updateFilterFromPad(x, y);
+    }
+  }
+
+  private updateFilterFromPad(x: number, y: number): void {
+    const canvas: HTMLCanvasElement = this.canvasfilter.nativeElement;
+    const frequency = x / canvas.width * 24000;
+    const qValue = (canvas.height / 2) - y;
+    //const qValue2 = 2 * qValue / canvas.height;    
+    //const qValue2 = 2 * ((canvas.height / 2) - y) / canvas.height;    
+    const qValue2 = (canvas.height - ( 2 * y)) / canvas.height;    
+    const q = this.MAX * (canvas.height - ( 2 * y)) / canvas.height;;
+    console.log(q);
     
+    this.biquadFilter.frequency.value = frequency;
+    //this.biquadFilter.frequency.setValueAtTime(frequency, this.audioCtx.currentTime);
+    this.biquadFilter.Q.value = q;
   }
+
 
 }
