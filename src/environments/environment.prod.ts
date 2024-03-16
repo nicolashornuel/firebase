88e0d49cce825b497cd8285b565f1ad00d701aca
Nicolas HORNUEL <nicolas.hornuel@gmail.com>
2024-03-16 10:33:49 +0100

    flat directory

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  discogsToken: "lFqQfxurDTjYekydiilIbmdglWEdZqWinZMEnugw",
+  youtubeToken: "AIzaSyDL_mFIhyWvHaC_auDh1xWpHMQ5nENbqnU",
+  urlApiDiscogs: "https://api.discogs.com",
+  urlApiYoutube: "https://www.googleapis.com/youtube/v3/search?",
+  //urlBack: "http://localhost:5001/fourthproject-aff75/us-central1/app/fire",
+  urlBack: "https://us-central1-fourthproject-aff75.cloudfunctions.net/app/fire",
+};
